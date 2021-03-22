b2207ba420fe0edf6536df8d368b9c4c1f4ec3ec
Thieles Martins <thielesmartins@MacBook-Air-de-Thieles.local>
2021-03-22 18:50:52 -0300

    ajuste

--- src/test/java/app/com/cadastro/CadastroApplicationTests.java
+++ src/test/java/app/com/cadastro/CadastroApplicationTests.java
@@ -1,9 +1,14 @@
 package app.com.cadastro;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CadastroApplicationTests.class)
+@TestPropertySource(locations="classpath:application.properties")
 class CadastroApplicationTests {
 
     @Test
